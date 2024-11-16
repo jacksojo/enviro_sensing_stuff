6f27a30e826841e7c269f4abe7223b29e826b3bc
jacksojo <57553946+jacksojo@users.noreply.github.com>
2024-11-15 21:45:57 -0800

    Update read_logs.py

--- read_logs.py
+++ read_logs.py
@@ -12,6 +12,21 @@ with open(todays_file, 'r') as file:
     for line in file:
         raw.append(line)
 
-print(raw[-1])
-
 file.close()
+
+schema = [
+   #['col_name', 'datetype', cleaning_func] 
+    ['log_timestamp', 'string', None],
+    ['log_source', 'string', None],
+    ['log_level', 'string', None],
+    ['temperature_c', 'float', lambda x: x[:-1]],
+    ['pressure_hpa', 'float', lambda x: x[:-3]],
+    ['humidity_%', lambda x: x[:-1]],
+    ['elevation_m', lambda x: x[:-1]],
+    ['date', 'string', None],
+    ['time', 'string', None]
+]
+
+df = pd.DataFrame([x.split() for x in raw], columns=[x[0] for x in schema])
+
+print(df.tail())
