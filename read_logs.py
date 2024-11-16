5558a8581af162e01568dcc502ebe871088cc5dd
jacksojo <57553946+jacksojo@users.noreply.github.com>
2024-11-15 21:34:21 -0800

    Update read_logs.py

--- read_logs.py
+++ read_logs.py
@@ -4,8 +4,14 @@ from datetime import date
 
 todays_file = f'logs/temps_{date.today()}.log'
 
-f = open(todays_file, "r")
+raw = []
 
-print(f.read()[0:100])
+# Open the file in read mode
+with open(todays_file, 'r') as file:
+    # Read each line in the file
+    for line in file:
+        raw.append(line)
+
+print(raw[-1])
 
 f.close()
