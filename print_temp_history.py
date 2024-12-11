373cc642e6c792e1ae368dfafd5ed2ca534ffca4
jacksojo <57553946+jacksojo@users.noreply.github.com>
2024-12-11 20:32:09 +1300

    Update print_temp_history.py

--- print_temp_history.py
+++ print_temp_history.py
@@ -4,4 +4,30 @@ import glob
 file_location = 'logs/'
 
 files = glob.glob(file_location+'*')
-print(files)
+
+raw = []
+
+# Open the file in read mode
+for file in files:
+    with open(file, 'r') as f:
+    # Read each line in the file
+        for line in f:
+            raw.append(line)
+
+schema = [
+   #['col_name', 'datetype', cleaning_func] 
+    ['log_timestamp', 'string', lambda x: x],
+    ['log_source', 'string', lambda x: x],
+    ['log_level', 'string', lambda x: x],
+    ['temperature_c', 'float', fix_float],
+    ['pressure_hpa', 'float', fix_float],
+    ['humidity_%', 'float', fix_float],
+    ['elevation_m', 'float', fix_float],
+    ['date', 'string', lambda x: x],
+    ['time', 'string', lambda x: x]
+]
+
+df = pd.DataFrame([x.split() for x in raw], columns=[x[0] for x in schema])
+
+print(df.head())
+
