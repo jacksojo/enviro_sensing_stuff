989a09011246c29e2b9ca83b35ea466390a05abf
jacksojo <57553946+jacksojo@users.noreply.github.com>
2024-12-11 21:38:09 +1300

    Update print_temp_history.py

--- print_temp_history.py
+++ print_temp_history.py
@@ -53,7 +53,7 @@ week_min = min([x[1] for x in date_ranges])
 week_max = max(x[2] for x in date_ranges)
 degrees_diff = math.floor(week_max) - math.floor(week_min)
 
-for deg in range(degrees_diff):
+for deg in range(degrees_diff+1):
     row = '  '
 
     for d in date_ranges:
