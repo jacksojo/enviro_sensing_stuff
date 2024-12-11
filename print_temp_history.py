7c5d9924cf5a14affc2d261509d1b31c220e3b6e
jacksojo <57553946+jacksojo@users.noreply.github.com>
2024-12-11 21:24:37 +1300

    Update print_temp_history.py

--- print_temp_history.py
+++ print_temp_history.py
@@ -46,12 +46,12 @@ print(df)
 other_rows = [x for x in raw if len(x.split()) != len(schema)]
 
 date_ranges = []
-for d in df['date'].dt.date.sort_values().unique()[-8:]:
+for d in df['date'].sort_values().unique()[-8:]:
     date_min = df['temperature_c'].loc[df['date'] == d].min()
     date_max = df['temperature_c'].loc[df['date'] == d].max()
     print(date_min, date_max)
     print(type(date_min))
-    date_ranges.append([d, date_min, date_max])
+    date_ranges.append([d.dt.date, date_min, date_max])
 
 week_min = min([x[1] for x in date_ranges])
 week_max = max(x[2] for x in date_ranges)
