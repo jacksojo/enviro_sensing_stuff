9a222abc00a9acec001ac42749c0bf74eaaabb8f
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-01-27 22:58:06 -0700

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,17 @@
+def send_email(content):
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_username = os.environ['SMTP_USERNAME']
+    smtp_password = os.environ['SMTP_PASSWORD']
+
+    msg = MIMEMultipart()
+    msg['From'] = os.environ['SMTP_USERNAME']
+    msg['To'] = os.environ['MY_EMAIL']
+    msg['Subject'] = "Temp Sensor Error"
+    msg.attach(MIMEText(repr(content), 'plain'))
+
+    # Send email
+    with smtplib.SMTP(smtp_server, smtp_port) as smtp:
+        smtp.starttls()
+        smtp.login(os.environ['SMTP_USERNAME'], os.environ['SMTP_PASSWORD'])
+        smtp.send_message(msg)
