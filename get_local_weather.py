7814167471aaec812404381fff7919bf59d3a524
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-01-27 23:25:43 -0700

    Create get_local_weather.py

---
+++ get_local_weather.py
@@ -0,0 +1,18 @@
+import requests
+import os
+import send_email
+import json
+
+postcode = 'V1A2Y9'
+
+api_key = os.environ['$FREE_WEATHER_API_KEY']
+url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={postcode}&aqi=yes'
+
+raw = requests.get(url, headers={'connection': 'close'})
+
+raw_json = json.loads(raw.body)
+
+try:
+  return raw
+except Exception as e:
+  raise e
