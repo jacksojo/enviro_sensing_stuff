a89c759a8e3bebba604775e5c7f638fa7cc594d3
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-10 12:06:45 -0700

    Update read_sensor.py

--- read_sensor.py
+++ read_sensor.py
@@ -44,7 +44,7 @@ while True:
     try:
         temperature, humidity, pressure = read_data()
         payload = {
-            'timestamp':{str(timestamp)},
+            'timestamp':str(timestamp),
             'temperature':temperature, 
             'humidity':humidity, 
             'pressure':pressure
