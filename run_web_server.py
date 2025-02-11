818a42a18668de4efb738ceb74330426419142cf
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-10 17:10:00 -0700

    Create run_web_server.py

---
+++ run_web_server.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+import time
+from db_utils import execute_query, BME280_TABLE_DEF
+
+app = Flask(__name__)
+
+data = jsonify(execute_query(f'select * from {BME280_TABLE_DEF} order by timestamp desc'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Accessible on the local network
