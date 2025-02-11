f5ac67416dc1fbd38229f03210b1c2970b43b42d
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-10 23:50:25 -0700

    Update display_data.py

--- display_data.py
+++ display_data.py
@@ -96,7 +96,7 @@ small = '.'+str(test_temp).split('.')[1][:2]+'°'
 small_w = small_font.getlength(small)
 
 temp_widget = widget(buffer,buffer,int(WIDTH)-buffer*2,int(HEIGHT/1.6)-buffer*2,(190,190,160))
-temp_widget.add_text(big,large_font,temp_widget.width-big_w-small_w-buffer,temp_widget.height-large_font_height)
+temp_widget.add_text(big,large_font,temp_widget.width-big_w-small_w-buffer,temp_widget.height-large_font_height+10)
 temp_widget.add_text(small,small_font,temp_widget.width-small_w-buffer,temp_widget.height-small_font_height)
 
 ## dummy data
@@ -117,7 +117,7 @@ hum_widget = widget(buffer,temp_widget.height+buffer*2,int(WIDTH/1.6)-buffer*2,i
 hum_widget.add_text(pre,small_font,buffer,buffer)
 hum_widget.add_text(pre_unit, very_small_font, small_font.getlength(pre)+buffer*2, small_font_height-very_small_font_height-buffer)
 hum_widget.add_text(hum,small_font,buffer,small_font_height)
-hum_widget.add_text(hum_unit, very_small_font, small_font.getlength(hum)+buffer*2, small_font_height-very_small_font_height-buffer)
+hum_widget.add_text(hum_unit, very_small_font, small_font.getlength(hum)+buffer*2, small_font_height+very_small_font_height+buffer)
 hum_widget.publish()
 
 
