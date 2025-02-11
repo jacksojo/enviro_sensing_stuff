91e85776ae00115a735bd11264cea7cc58dbdd22
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-11 08:17:37 -0700

    update

--- display_data.py
+++ display_data.py
@@ -89,7 +89,7 @@ class widget:
     img.paste(self.image, (self.x, self.y))
 
 data = execute_query(f"select * from {BME280_TABLE_DEF['table_name']} where timestamp >= {str(datetime.date.today())}")
-print(f"select * from {BME280_TABLE_DEF['table_name']} where timestamp >= {str(datetime.date.today())"}
+print(f"select * from {BME280_TABLE_DEF['table_name']} where timestamp >= {str(datetime.date.today())}")
 print('first_row', data[0])
 print('last_row', data[-1])
 current_temp = data[-1]['temperature']
