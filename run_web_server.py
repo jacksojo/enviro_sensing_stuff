04c0fca76e4db74061b2bd8926adc0f7f071d298
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-10 17:33:31 -0700

    Update run_web_server.py

--- run_web_server.py
+++ run_web_server.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 
 def get_data():
     with app.app_context():
-        data = jsonify(execute_query(f"select * from {BME280_TABLE_DEF['table_name']} order by timestamp desc"))
+        data = str(execute_query(f"select * from {BME280_TABLE_DEF['table_name']} order by timestamp desc"))
+        print(data)
         return data
 
 @app.route("/")
 def home():
     x = get_data()
-    print(str(x))
-    return f"Data: {str(x)}"
+
+    return f"Data: {x}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)  # Accessible on the local network
