cf01eb6c79aae3c29155bee43f8285aa24f49ec5
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-10 17:15:51 -0700

    Update run_web_server.py

--- run_web_server.py
+++ run_web_server.py
@@ -4,7 +4,8 @@ from db_utils import execute_query, BME280_TABLE_DEF
 
 app = Flask(__name__)
 
-data = jsonify(execute_query(f"select * from {BME280_TABLE_DEF['table_name']} order by timestamp desc"))
+with app.app_context():
+    data = jsonify(execute_query(f"select * from {BME280_TABLE_DEF['table_name']} order by timestamp desc"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Accessible on the local network
