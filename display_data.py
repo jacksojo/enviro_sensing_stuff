181478c7ccdc38442011b89ad83d60f999c9f821
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-11 16:10:52 -0700

    Update display_data.py

--- display_data.py
+++ display_data.py
@@ -139,8 +139,8 @@ def build_image(disp):
   minutes_w = small_font.getlength(minutes)
   
   time_widget = widget(hum_widget.width+buffer*2, temp_widget.height+buffer*2, disp_width-hum_widget.width-buffer*3, disp_height-temp_widget.height-buffer*3,color=(200,200,255,0))
-  time_widget.add_text(hours, small_font, time_widget.width/2-hours_w/2, time_widget.height*.25-small_font_size/2)
-  time_widget.add_text(minutes, small_font, time_widget.width/2-minutes_w/2, time_widget.height*.75-small_font_size/2)
+  time_widget.add_text(hours, small_font, time_widget.width/2-hours_w/2, time_widget.height*.25-small_font_height/2)
+  time_widget.add_text(minutes, small_font, time_widget.width/2-minutes_w/2, time_widget.height*.75-small_font_height/2)
   time_widget.publish()            
 
   return img
