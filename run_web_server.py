e32f663068b58d3e0a6d453b62aa5fdb109dc660
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-10 18:06:41 -0700

    Update run_web_server.py

--- run_web_server.py
+++ run_web_server.py
@@ -4,10 +4,10 @@ from db_utils import execute_query, BME280_TABLE_DEF
 
 app = Flask(__name__)
 
-def get_data():
-    with app.app_context():
-        data = str(execute_query(f"select * from {BME280_TABLE_DEF['table_name']} order by timestamp desc")[0])
-        return data
+#def get_data():
+#    with app.app_context():
+#        data = str(execute_query(f"select * from {BME280_TABLE_DEF['table_name']} order by timestamp desc")[0])
+#        return data
 
 @app.route("/image")
 def serve_image():
