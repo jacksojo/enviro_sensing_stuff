4a70c7ed3dab599452ae569fe5f1c442ade498ba
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-18 11:11:56 -0700

    Update display_data.py

--- display_data.py
+++ display_data.py
@@ -95,13 +95,13 @@ def build_image(disp):
 
       main_line = norm_data(data)
 
-      print('main',main_line[0])
+      print('main',main_line[0:10])
 
       if shadow_data:
         shadow_line = norm_data(shadow_data)
         self.draw.line(shadow_data,fill=(150,150,150,255),width=1)
 
-        print('shadow',shadow_line[0])
+        print('shadow',shadow_line[0:10])
                 
       self.draw.line(main_line,fill=color,width=weight)
 
