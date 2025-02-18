52b9e194b31587cda241ae00b9a6133df4dcffc7
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-18 10:49:22 -0700

    Update display_data.py

--- display_data.py
+++ display_data.py
@@ -95,9 +95,9 @@ def build_image(disp):
       img.paste(self.image, (self.x, self.y))
 
   def get_data():
-    q_template = f"""
+    q_template = """
       SELECT *
-      FROM {BME280_TABLE_DEF['table_name']}
+      FROM {table_name}
       WHERE TIMESTAMP >= '{start}' AND < '{end}'
       """
     today = datetime.date.today()
@@ -105,8 +105,8 @@ def build_image(disp):
     tomorrow = today + datetime.timedelta(days=1)
     yesterday = today - datetime.timedelta(days=1)
     
-    from_today = execute_query(q_template.format(start=str(today), end=str(tomorrow)))
-    from_yesterday = execute_query(q_template.format(start=str(yesterday), end=str(this_time_yesterday)))
+    from_today = execute_query(q_template.format(start=str(today), end=str(tomorrow), table_name=BME280_TABLE_DEF['table_name']))
+    from_yesterday = execute_query(q_template.format(start=str(yesterday), end=str(this_time_yesterday), table_name=BME280_TABLE_DEF['table_name']))
 
     return from_today, from_yesterday
 
