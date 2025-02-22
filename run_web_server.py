f4c2a70b1668c4c2568366abca6be55ec9ab0727
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-22 14:44:40 -0700

    Update run_web_server.py

--- run_web_server.py
+++ run_web_server.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/image")
 def serve_image():
-    image_path = SCRIPT_DIR / "/data/latest_image.png"
+    image_path = SCRIPT_DIR / "data" / "latest_image.png"
     return send_file(image_path, mimetype='image/png')
 
 if __name__ == '__main__':
