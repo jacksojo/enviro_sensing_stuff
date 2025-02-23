3e188050446486fa4332244fcf89d226bf4ba2df
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-23 15:03:26 -0700

    Update test_stuff.py

--- test_stuff.py
+++ test_stuff.py
@@ -1,21 +1,15 @@
 import read_sensor
 import time
-from MPU6050 import MPU6050
+mpu6050 import mpu6050
 
 # Pass your MPU6050 Address
-mpu = MPU6050.MPU6050(0x68)
-
-# Init Settings
-mpu.reset()
-mpu.power_manage()
-mpu.gyro_config()
-mpu.accel_config()
+mpu = mpu6050(0x68)
 
 bme280 = read_sensor.init_sensor()
 
 while True:
    # Temperature Readings in Celsius
-  print(f"mpu6050 Temperature values are {mpu.get_temperature()}")
+  print(sensor.get_accel_data())
   
   read_sensor.read_data(bme280, write_to_db=False)
 
