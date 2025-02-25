fc97450c58fa682ec24efd4367b6c83e96b6b52e
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-02-25 15:40:56 -0700

    Update run_web_server.py

--- run_web_server.py
+++ run_web_server.py
@@ -17,4 +17,4 @@ def serve_image():
     return send_file(image_path, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)  # Accessible on the local network
+    app.run(host='0.0.0.0', port=5000, debug=False)  # Accessible on the local network
