cf8a649327af9bc778b754f8518520b2beb4c85c
jacksojo <jonathan.jackson32@gmail.com>
2025-02-26 10:38:24 -0700

    Refactor test_stuff.py to use motion sensor module

--- test_stuff.py
+++ test_stuff.py
@@ -1,17 +1,9 @@
-import read_sensor
+import motion_sensor
 import time
-from mpu6050 import mpu6050
-import smbus
 
-bus = smbus.SMBus(0)
-
-# Pass your MPU6050 Address
-mpu = mpu6050(0x68, bus=0)
-
-bme280 = read_sensor.init_sensor()
+motion_line = motion_sensor.init_motion_sensor()
 
 while True:
-
-  print(mpu.get_gyro_data())
-
+  reading = motion_sensor.check_motion(motion_line)
+  print('motion: ', reading)
   time.sleep(1)
