b8ea58b3e33aaa47c22353a8b4ce4996cf0ee1ab
jacksojo <jonathan.jackson32@gmail.com>
2025-02-26 21:58:28 -0700

    bug fix

--- motion_sensor.py
+++ motion_sensor.py
@@ -2,7 +2,10 @@ import RPi.GPIO as GPIO
 
 MOTION_SENSOR_PIN = 17
 
-def check_motion():
+def init_motion_sensor():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(MOTION_SENSOR_PIN, GPIO.IN)
-    return GPIO.input(MOTION_SENSOR_PIN) == GPIO.HIGH 
+    return MOTION_SENSOR_PIN
+
+def check_motion(pin):
+    return GPIO.input(pin) == GPIO.HIGH 
