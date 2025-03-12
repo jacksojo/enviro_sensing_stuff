d7a944ba5a4ecc303686c62e2b10c62273277209
jacksojo <57553946+jacksojo@users.noreply.github.com>
2025-03-11 23:08:31 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import st7789
+import display_data
+from PIL import Image
+
+display = display_data.init_display()
+
+path = r'data/latest_image.png'
+
+img = Image.open(path)
+
+if image:
+  print('image found')
+else
+  print('image read error').convert("RGB")
+  
+pixels = img.load()
+
+display_data.display_image_on_screen(display, img)
+
